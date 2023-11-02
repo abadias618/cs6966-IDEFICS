00cc771fc94ea1ebc41263a82e9a3c1008c77c8d
Abadias618 <abadias618@gmail.com>
2023-11-02 13:40:45 -0600

    fix f1_score syntax

--- utils.py
+++ utils.py
@@ -56,7 +56,7 @@ def make_batch_of_prompts(images: list[Image.Image]) -> list:
     return prompts
 
 # https://kierszbaumsamuel.medium.com/f1-score-in-nlp-span-based-qa-task-5b115a5e7d41
-def f1_score(str: gold, str: pred)-> float:
+def f1_score(gold: str, pred: str) -> float:
     """
     Get f1_score comparing gold standard and prediction.
     """

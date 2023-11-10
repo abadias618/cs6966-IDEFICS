7f148c68432366155d5eb948eef81cb6c576ab94
jakobottar <jakobottar@pm.me>
2023-11-10 15:48:40 -0700

    remove ntfy script and edit training set size in config

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=32
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
-#SBATCH --time=0:10:00
+#SBATCH --time=0:20:00
 #SBATCH --mem=65GB
 #SBATCH -o proj-%j.log
 
@@ -14,8 +14,8 @@ conda activate le-proj
 export DOWNLOAD_DIR="/scratch/general/vast/$USER/huggingface_cache"
 export TRANSFORMERS_CACHE="/scratch/general/vast/$USER/huggingface_cache"
 export HF_DATASETS_CACHE="/scratch/general/vast/$USER/huggingface_cache"
-export TRANSFORMERS_OFFLINE=1
+export TRANSFORMERS_OFFLINE=1 # cache huggingface stuff offline (it's api sucks ass)
 export HF_DATASETS_OFFLINE=1
 
 OUT_DIR=/scratch/general/vast/$USER/cs6966/proj/models
-ntfy $SLURM_JOB_ID python main.py -o ${OUT_DIR}
+python main.py -o ${OUT_DIR}
